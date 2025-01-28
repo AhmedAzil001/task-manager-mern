71e056f9bd4091e9e30e4c7be5287dffd5dddd04
Azil Ahmed <azilahmed2016@gmail.com>
2025-01-28 22:30:06 +0530

    completed the auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/config";
+
+export default auth = (req, res, next) => {
+  const authorization = req.headers["authorization"];
+  if (!authorization || !authorization.startsWith("Bearer")) {
+    return res.status(401).json({
+      message: "Unauthorised : no token found",
+    });
+  }
+  try {
+    const token = authorization.split(" ")[1];
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({
+        message: "Invalid token",
+      });
+    }
+    req.userId = decoded._id;
+    next();
+  } catch (error) {
+    return res.status(500).status({
+      message: error.message,
+    });
+  }
+};
